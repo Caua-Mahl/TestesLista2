cea0fc7c0ae394e0ae751572fa2af9f50e76ba35
Cauã Mahl <cauamahl@yahoo.com.br>
2024-02-22 11:41:37 -0300

    v4
    falta ajeitar o clean code e melhorar o corrigir

--- src/CalculadoraFinanceira.php
+++ src/CalculadoraFinanceira.php
@@ -8,103 +8,110 @@ class CalculadoraFinanceira
 {
     public function calcularJurosSimples($capital, $taxa, $tempo): string
     {
-        try 
-        {
+        try {
             $this->validar($capital, $taxa, $tempo);
 
             return 'R$ ' . number_format(($capital * $taxa * 0.01 * $tempo), 2, ',', '.');
-        } 
-        catch (Exception $e) 
-        {
+        } catch (Exception $e) {
             return $e->getMessage();
         }
     }
 
     public function calcularJurosCompostos($capital, $taxa, $tempo): string
     {
-        try 
-        {
+        try {
             $this->validar($capital, $taxa, $tempo);
 
             return 'R$ ' . number_format($capital * (1 + $taxa * 0.01) ** $tempo, 2, ',', '.');
-        } 
-        catch (Exception $e) 
-        {
+        } catch (Exception $e) {
             return $e->getMessage();
         }
     }
 
     public function calcularAmortizacao($capital, $taxa, $tempo, $tipo): array | string
     {
-        try 
-        {
+        try {
             $this->validar($capital, $taxa, $tempo, $tipo);
 
-            $juros     = array();
-            $resultado = array();
-            $taxa      = $taxa * 0.01;
+            $taxa = $taxa * 0.01;
 
-            if ($tipo == "SAC") 
-            {
-                $amortizacao = number_format($capital / $tempo, 2, ',', '.');
+            if ($tipo == "SAC") {
+                $amortizacao = $capital / $tempo;
 
-                for ($i = 0; $i < $tempo; $i++) 
-                {
-                    $juros[$i]     = number_format($capital * $taxa - $capital / $tempo * $i * ($taxa) , 2, ',', '.');
-                    $resultado[$i] = array( "Amortização" => 'R$ ' . $amortizacao,
-                                            "Juros"       => 'R$ ' . $juros[$i]);
-                }     
+                for ($i = 0; $i < $tempo; $i++) {
+                    $juros[$i]        = $capital     * $taxa        - $capital / $tempo * $i * ($taxa);
+                    $devedor[$i]      = $capital     - $amortizacao * ($i + 1);
+                    $valorParcela[$i] = $amortizacao + $juros[$i];
+                    $resultado[$i]    = [
+                        "Parcela"     => $i + 1,
+                        "Valor"       => 'R$ ' . number_format($valorParcela[$i], 2, ',', '.'),
+                        "Amortização" => 'R$ ' . number_format($amortizacao,      2, ',', '.'),
+                        "Juros"       => 'R$ ' . number_format($juros[$i],        2, ',', '.'),
+                        "Devedor"     => 'R$ ' . number_format($devedor[$i],      2, ',', '.')
+                    ];
+                }
+
+                $resultado["Total"]   = [
+                    "Valor"       => 'R$ ' . number_format(array_sum($valorParcela), 2, ',', '.'),
+                    "Amortização" => 'R$ ' . number_format($capital, 2, ',', '.'),
+                    "Juros"       => 'R$ ' . number_format(array_sum($juros), 2, ',', '.')
+                ];
+
                 return $resultado;
             }
 
-            if ($tipo == "PRICE") 
-            {
-                $prestacao   = $capital * $taxa / (1 - (1 + $taxa) ** -$tempo);
-                $amortizacao = array();
+            //A fórmula é: P = (PV * i) / (1 – (1 + i)^-n), onde: P é a parcela, PV é o valor principal, i é a taxa de juros e.
+            if ($tipo == "PRICE") {
 
-                for ($i = 0; $i < $tempo; $i++) 
-                {
-                    $juros[$i]       = $capital * $taxa - $prestacao;
-                    $amortizacao[$i] = $prestacao - $juros[$i];
+                $devedor[0] = $capital;
 
-                    $resultado[$i]   = array( "Amortização" => 'R$ ' . number_format($amortizacao[$i], 2, ',', '.'),
-                                              "Juros"       => 'R$ ' . number_format($juros[$i], 2, ',', '.'));
-                }     
+                for ($i = 0; $i < $tempo; $i++) {
+                    $valorParcela  = ($capital * $taxa) / (1 - (1 + $taxa) ** -$tempo);
+                    $juros[$i] = $devedor[$i] * $taxa;
+                    $amortizacao[$i] = $valorParcela - $juros[$i];
+                    $devedor[$i] = $capital - $amortizacao[$i];
+                    $devedor[$i+1] = $capital - $amortizacao[$i];
+                    $resultado[$i]   =  [
+                        "Parcela"     => $i + 1,
+                        "Valor"       => 'R$ ' . number_format($valorParcela, 2, ',', '.'),
+                        "Amortização" => 'R$ ' . number_format($amortizacao[$i],      2, ',', '.'),
+                        "Juros"       => 'R$ ' . number_format($juros[$i],        2, ',', '.'),
+                        "Devedor"     => 'R$ ' . number_format($devedor[$i],      2, ',', '.')
+                    ];
+                }
+                /*
+                $resultado["Total"]   = [
+                    "Valor"       => 'R$ ' . number_format(array_sum($valorParcela), 2, ',', '.'),
+                    "Amortização" => 'R$ ' . number_format($capital, 2, ',', '.'),
+                    "Juros"       => 'R$ ' . number_format(array_sum($juros), 2, ',', '.')
+                ]; */
                 return $resultado;
             }
-        } 
-        catch (Exception $e) 
-        {
+        } catch (Exception $e) {
             return $e->getMessage();
         }
     }
 
     private function validar($capital, $taxa, $tempo, $tipo = "SAC")
     {
-        if ($capital < 0) 
-        {
+        if ($capital < 0) {
             throw new Exception("O capital deve ser positivo");
         }
 
-        if ($taxa < 0) 
-        {
+        if ($taxa < 0) {
             throw new Exception("A taxa deve ser positiva");
         }
 
-        if ($tempo < 0) 
-        {
+        if ($tempo < 0) {
             throw new Exception("O tempo deve ser positivo");
         }
 
-        if (!is_numeric($capital) || !is_numeric($taxa) || !is_numeric($tempo) || !is_string($tipo))
-        {
+        if (!is_numeric($capital) || !is_numeric($taxa) || !is_numeric($tempo) || !is_string($tipo)) {
             throw new Exception("Argumento(s) invalido(s)");
         }
 
-        if ($tipo != "SAC" && $tipo != "PRICE") 
-        {
+        if ($tipo != "SAC" && $tipo != "PRICE") {
             throw new Exception("Tipo inválido, escolha entre 'SAC' ou 'PRICE'");
         }
-
     }
 }
--- tests/CalculadoraFinanceiraTest.php
+++ tests/CalculadoraFinanceiraTest.php
@@ -15,10 +15,16 @@ class CalculadoraFinanceiraTest extends TestCase
     {
         $calculadora = new CalculadoraFinanceira();
         $esperado    = 'R$ 262,50';
+        $esperado2   = 'R$ 63.000.000.000,00';
+        $esperado3   = 'R$ 0,00';
 
         $resultado   = $calculadora->calcularJurosSimples(350, 15, 5);
+        $resultado2  = $calculadora->calcularJurosSimples(1000000000, 700, 9);
+        $resultado3  = $calculadora->calcularJurosSimples(0, 2, 2);
 
-        $this->assertSame($esperado, $resultado);
+        $this->assertSame($esperado, $resultado,   "erro no juros simples");
+        $this->assertSame($esperado2, $resultado2, "erro no juros simples limite alto");
+        $this->assertSame($esperado3, $resultado3, "erro no juros simples limite baixo");
     }
 
     public function testCalcularJurosSimplesComValoresNegativos()
@@ -29,8 +35,8 @@ class CalculadoraFinanceiraTest extends TestCase
         $esperado3    = 'O tempo deve ser positivo';
 
         $resultado1   = $calculadora->calcularJurosSimples(-350,  15,  5);
-        $resultado2   = $calculadora->calcularJurosSimples( 350, -15,  5);
-        $resultado3   = $calculadora->calcularJurosSimples( 350,  15, -5);
+        $resultado2   = $calculadora->calcularJurosSimples(350, -15,  5);
+        $resultado3   = $calculadora->calcularJurosSimples(350,  15, -5);
 
         $this->assertEquals($esperado1, $resultado1, "erro ao testar capital negativo");
         $this->assertEquals($esperado2, $resultado2, "erro ao testar taxa negativa");
@@ -46,14 +52,23 @@ class CalculadoraFinanceiraTest extends TestCase
 
         $this->assertEquals($esperado, $resultado);
     }
+
     public function testCalcularJurosCompostos()
     {
         $calculadora = new CalculadoraFinanceira();
         $esperado    = 'R$ 703,98';
+        $esperado2   = 'R$ 134.217.728.000.000.000,00';
+        $esperado3   = 'R$ 0,00';
+
 
         $resultado   = $calculadora->calcularJurosCompostos(350, 15, 5);
+        $resultado2  = $calculadora->CalcularJurosCompostos(1000000000, 700, 9);
+        $resultado3  = $calculadora->CalcularJurosCompostos(0, 2, 2);
 
-        $this->assertEquals($esperado, $resultado);
+
+        $this->assertEquals($esperado,  $resultado,  "erro no juros compostos");
+        $this->assertEquals($esperado2, $resultado2, "erro no juros compostos limite alto");
+        $this->assertEquals($esperado3, $resultado3, "erro no juros compostos limite baixo");
     }
 
     public function testCalcularJurosCompostosComValoresNegativos()
@@ -64,13 +79,13 @@ class CalculadoraFinanceiraTest extends TestCase
         $esperado3    = 'O tempo deve ser positivo';
 
         $resultado1   = $calculadora->calcularJurosCompostos(-350,  15,  5);
-        $resultado2   = $calculadora->calcularJurosCompostos( 350, -15,  5);
-        $resultado3   = $calculadora->calcularJurosCompostos( 350,  15, -5);
+        $resultado2   = $calculadora->calcularJurosCompostos(350, -15,  5);
+        $resultado3   = $calculadora->calcularJurosCompostos(350,  15, -5);
 
         $this->assertEquals($esperado1, $resultado1, "erro ao testar capital negativo");
         $this->assertEquals($esperado2, $resultado2, "erro ao testar taxa negativa");
         $this->assertEquals($esperado3, $resultado3, "erro ao testar tempo negativo");
-    } 
+    }
 
     public function testCalcularJurosCompostosComValoresInvalidos()
     {
@@ -85,18 +100,16 @@ class CalculadoraFinanceiraTest extends TestCase
     public function testCalcularAmortizacaoSAC()
     {
         $calculadora  = new CalculadoraFinanceira();
-        $esperado     = array(
-                               array('Amortização' => 'R$ 5.000,00', 'Juros' => 'R$ 175,00'),
-                               array('Amortização' => 'R$ 5.000,00', 'Juros' => 'R$ 140,00'),
-                               array('Amortização' => 'R$ 5.000,00', 'Juros' => 'R$ 105,00'),
-                               array('Amortização' => 'R$ 5.000,00', 'Juros' => 'R$ 70,00'),
-                               array('Amortização' => 'R$ 5.000,00', 'Juros' => 'R$ 35,00')
-                            );
+        $esperado     =[['Parcela' => 1, 'Valor' => 'R$ 5.175,00',  'Amortização' => 'R$ 5.000,00',  'Juros' => 'R$ 175,00', 'Devedor' => 'R$ 20.000,00'],
+                        ['Parcela' => 2, 'Valor' => 'R$ 5.140,00',  'Amortização' => 'R$ 5.000,00',  'Juros' => 'R$ 140,00', 'Devedor' => 'R$ 15.000,00'],
+                        ['Parcela' => 3, 'Valor' => 'R$ 5.105,00',  'Amortização' => 'R$ 5.000,00',  'Juros' => 'R$ 105,00', 'Devedor' => 'R$ 10.000,00'],
+                        ['Parcela' => 4, 'Valor' => 'R$ 5.070,00',  'Amortização' => 'R$ 5.000,00',  'Juros' => 'R$ 70,00',  'Devedor' => 'R$ 5.000,00'],
+                        ['Parcela' => 5, 'Valor' => 'R$ 5.035,00',  'Amortização' => 'R$ 5.000,00',  'Juros' => 'R$ 35,00',  'Devedor' => 'R$ 0,00'],
+                         'Total'   =>   ['Valor' => 'R$ 25.525,00', 'Amortização' => 'R$ 25.000,00', 'Juros' => 'R$ 525,00']];
 
         $resultado     = $calculadora->calcularAmortizacao(25000.00, 0.7, 5, "SAC");
 
-        assertEquals($esperado, $resultado, "erro no SAC 1");       
-
+        assertEquals($esperado, $resultado, "erro no SAC 1");
     }
 
     public function testCalcularAmortizacaoComValoresNegativos()
@@ -107,8 +120,8 @@ class CalculadoraFinanceiraTest extends TestCase
         $esperado3    = 'O tempo deve ser positivo';
 
         $resultado1   = $calculadora->calcularAmortizacao(-25000.00,  0.7,  5, "SAC");
-        $resultado2   = $calculadora->calcularAmortizacao( 25000.00, -0.7,  5, "SAC");
-        $resultado3   = $calculadora->calcularAmortizacao( 25000.00,  0.7, -5, "SAC");
+        $resultado2   = $calculadora->calcularAmortizacao(25000.00, -0.7,  5, "SAC");
+        $resultado3   = $calculadora->calcularAmortizacao(25000.00,  0.7, -5, "SAC");
 
         $this->assertEquals($esperado1, $resultado1, "erro ao testar capital negativo");
         $this->assertEquals($esperado2, $resultado2, "erro ao testar taxa negativa");
@@ -135,19 +148,20 @@ class CalculadoraFinanceiraTest extends TestCase
         $this->assertEquals($esperado, $resultado);
     }
 
-   /* public function testCalcularAmortizacaoPrice()
+    public function testCalcularAmortizacaoPrice()
     {
-        $calculadora   = new CalculadoraFinanceira();
-        $esperado      = array(
-                                array('Amortização' => 'R$ 4.930,49', 'Juros' => 'R$ 175,00'), 
-                                array('Amortização' => 'R$ 4.965,00', 'Juros' => 'R$ 140,49'),
-                                array('Amortização' => 'R$ 4.999,76', 'Juros' => 'R$ 105,73'),
-                                array('Amortização' => 'R$ 5.034,76', 'Juros' => 'R$ 70,73'),
-                                array('Amortização' => 'R$ 5.070,00', 'Juros' => 'R$ 35,49')  
-                            );
+        $calculadora  = new CalculadoraFinanceira();
+        $esperado     = [
+            ['Parcela' => 1, 'Valor' => 'R$ 5.105,49',  'Amortização' => 'R$ 4.930,49',  'Juros' => 'R$ 175,00', 'Devedor' => 'R$ 20.069,51'],
+            ['Parcela' => 2, 'Valor' => 'R$ 5.105,49',  'Amortização' => 'R$ 4.965,00',  'Juros' => 'R$ 140,49', 'Devedor' => 'R$ 15.104,51'],
+            ['Parcela' => 3, 'Valor' => 'R$ 5.105,49',  'Amortização' => 'R$ 4.999,76',  'Juros' => 'R$ 105,73', 'Devedor' => 'R$ 10.104,75'],
+            ['Parcela' => 4, 'Valor' => 'R$ 5.105,49',  'Amortização' => 'R$ 5.034,76',  'Juros' => 'R$ 70,73',  'Devedor' => 'R$ 5.070,00'],
+            ['Parcela' => 5, 'Valor' => 'R$ 5.105,49',  'Amortização' => 'R$ 5.070,00',  'Juros' => 'R$ 35,49',  'Devedor' => 'R$ 0,00'],
+            // 'Total'   =>    ['Valor' => 'R$ 25.527,45', 'Amortização' => 'R$ 25.000,00', 'Juros' => 'R$ 527,45']
+        ];
 
-        $resultado     = $calculadora->calcularAmortizacao(25000.00, 0.7, 5,"PRICE");
-    
+        $resultado     = $calculadora->calcularAmortizacao(25000.00, 0.7, 5, "PRICE");
+
         assertEquals($esperado, $resultado, "erro no PRICE 1");
-    }*/
+    }
 }
